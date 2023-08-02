5a9c0ad447982c1b5d6c86252af03c6ac33ec4cd
Lucas Vieira <59543597+lkasvr@users.noreply.github.com>
2023-08-02 17:26:25 -0300

    ajuste de tipo no hook useDimensions

--- src/hooks/use-dimensions.ts
+++ src/hooks/use-dimensions.ts
@@ -4,11 +4,11 @@ import { MutableRefObject, useEffect, useRef } from 'react';
 // a window or resize listener. Also use state/layoutEffect instead of ref/effect
 // if this is important to know on initial client render.
 // It would be safer to  return null for unmeasured states.
-export const useDimensions = (ref: MutableRefObject<HTMLElement>) => {
+export const useDimensions = (ref: MutableRefObject<HTMLElement | null>) => {
   const dimensions = useRef({ width: 0, height: 0 });
 
   useEffect(() => {
-    if (ref.current) {
+    if (ref.current instanceof HTMLElement) {
       dimensions.current.width = ref.current.offsetWidth;
       dimensions.current.height = ref.current.offsetHeight;
     }
