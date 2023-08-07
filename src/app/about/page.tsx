624ce2587fb59d817b2bc4d45f5049e7bc0e224b
Lucas Vieira <59543597+lkasvr@users.noreply.github.com>
2023-08-07 12:10:25 -0300

    Responsividade da página de escritório concluída

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -78,10 +78,10 @@ const items = [
 
 export default function Page() {
   return (
-    <div className="w-full h-full flex flex-row flex-wrap 2xl:flex-nowrap justify-center overflow-auto max-2xl:scrollbar-none">
-      <article className="p-4 2xl:p-9 w-full h-full 2xl:w-2/5 flex flex-row flex-wrap justify-center content-center gap-0 text-white">
-        <h2 className="mb-6 mt-6 ml-4 text-center text-lg">
-          <span className="mt-4">Somos o escritório</span>&nbsp;
+    <div className="w-full h-full flex flex-row flex-wrap 2xl:flex-nowrap justify-center gap-y-32 sm:gap-y-0 overflow-auto scrollbar-none 2xl:overflow-hidden">
+      <article className="p-4 2xl:p-9 w-full h-full 2xl:w-2/5 flex flex-row flex-wrap justify-center text-white">
+        <h2 className="mb-6 ml-4 text-center text-lg">
+          <span>Somos o escritório</span>&nbsp;
           <span className="md:text-xl font-bold">
             OLIVEIRA & RIOS &ndash; ADVOGADOS
           </span>
@@ -135,25 +135,33 @@ export default function Page() {
           ))}
         </Swiper>
       </article>
-      <hr className="w-10/12 h-[1px]  my-4 2xl:w-[1px] 2xl:h-2/3 place-self-center bg-white" />
-      <div className="p-4 2xl:p-9 w-full h-full 2xl:w-[66.6%] flex flex-row flex-wrap justify-center items-start">
-        <a href="#" className="block mt-2">
+      <hr className="w-10/12 h-[1px] my-4 2xl:w-[1px] 2xl:h-full bg-white" />
+      <div className="p-4 2xl:p-9 w-full h-full 2xl:w-[66.6%] flex flex-row flex-wrap justify-center">
+        <div className="mb-6 max-h-max flex flex-row flex-wrap justify-center">
           <Image
             alt="Brasil 21"
             src="/assets/images/brasil21_diurna.jpg"
             width={4110}
             height={2740}
-            className="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+            priority
+            className="h-56 w-11/12 rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
           />
 
-          <div className="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4 text-white">
+          <div className="mt-2 sm:flex sm:items-center sm:justify-center sm:gap-4 text-white">
             <strong className="font-medium">Complexo Brasil 21</strong>
 
             <span className="hidden sm:block sm:h-[2px] sm:w-8 sm:bg-secondary"></span>
 
             <p className="mt-0.5 opacity-50 sm:mt-0">Plano Piloto</p>
           </div>
-        </a>
+        </div>
+
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3839.213940049345!2d-47.8938863!3d-15.7926693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a3b1cad790387%3A0xaccac326c5d6d844!2sSetor%20Hoteleiro%20Sul%2C%20Quadra%2006%2C%20Edif%C3%ADcio%20Brasil%2021%2C%20Bloco%20A%2C%20Sala%20501!5e0!3m2!1spt-BR!2sus!4v1691416903640!5m2!1spt-BR!2sus"
+          className="w-[98%] h-[45%]"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </div>
   );
