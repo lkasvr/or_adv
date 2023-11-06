5591df0d02560b5b32e9f9e73718212fb817051d
Lucas Vieira <59543597+lkasvr@users.noreply.github.com>
2023-11-06 19:03:09 -0300

    Ajuste de responsividade da página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,20 @@ export default async function Home() {
   const articlesPreview = await getArticlesPreview();
 
   return (
-    <div className="md:p-4 xl:p-5 2xl:p-10 w-full h-full flex flex-row flex-nowrap justify-center items-center">
+    <div className="md:px-4 xl:px-5 2xl:p-10 w-full h-full flex flex-row flex-nowrap justify-center items-center max-md:rounded-b-3xl overflow-hidden">
       {/* H1 FOR SEO */}
       <h1 className="hidden">Oliveira e Rios Advogados</h1>
-      <div className="home-page mt-14 md:mt-0 w-full h-full flex flex-row flex-wrap 2xl:flex-nowrap rounded-3xl overflow-auto">
+      <div className="home-page mt-28 md:mt-0 w-full h-full flex flex-row flex-wrap 2xl:flex-nowrap rounded-3xl overflow-auto">
         {/* SECTION 1 */}
-        <section className="sub-section-1 relative px-4 py-8 md:px-16 xl:px-20 w-full 2xl:w-1/2 h-full flex flex-row flex-wrap content-center ">
+        <section className="sub-section-1 relative px-4 py-8 md:px-16 xl:px-20 w-full 2xl:w-1/2 h-full flex flex-row flex-wrap content-center">
           <Image
             src="/assets/images/salao_n_stj.jpg"
             alt="Salão Nobre STJ"
             width={1000}
             height={1000}
-            className="absolute w-full h-full top-0 left-0 rounded-t-3xl xl:rounded-e-none xl:rounded-s-3xl"
+            className="absolute w-full h-full top-0 left-0"
           />
-          <div className="absolute w-full h-full top-0 left-0 z-[1] bg-gradient-to-b xl:bg-gradient-to-r from-white/30 to-white/100 rounded-t-3xl xl:rounded-e-none xl:rounded-s-3xl"></div>
+          <div className="absolute w-full h-full top-0 left-0 z-[1] bg-gradient-to-b xl:bg-gradient-to-r from-white/30 to-white/100"></div>
           <h2 className="text-3xl xl:text-5xl text-primary font-extrabold leading-none tracking-tight z-[2]">
             Somos um escritório de advocacia
           </h2>
@@ -32,9 +32,10 @@ export default async function Home() {
           </span>
         </section>
         {/* SECTION 2 */}
-        <section className="sub-section-2 p-4 md:px-16 w-full 2xl:w-1/2 h-full bg-white flex flex-col content-center items-center">
-          <SectionPreview articlesPreview={articlesPreview} />
-        </section>
+        <SectionPreview
+          articlesPreview={articlesPreview}
+          wrapperClass="sub-section-2 px-3 py-10 md:px-10 2xl:w-1/2 h-full bg-white"
+        />
       </div>
     </div>
   );
--- src/components/Article/SectionPreview/index.tsx
+++ src/components/Article/SectionPreview/index.tsx
@@ -6,11 +6,12 @@ import Preview from '../Preview';
 
 interface Props {
   articlesPreview: ArticlePreview[];
+  wrapperClass?: string;
 }
 
-const SectionPreview = ({ articlesPreview }: Props) => {
+const SectionPreview = ({ articlesPreview, wrapperClass }: Props) => {
   return (
-    <div className="relative scrollbar-none overflow-auto">
+    <div className={`relative scrollbar-none overflow-auto ${wrapperClass}`}>
       {articlesPreview?.length > 1 && (
         <div className="max-2xl:hidden absolute top-0 left-0 w-full flex flex-row flex-nowrap justify-end">
           <ScrollDownIcon text="role para baixo" initialY={25} targetY={50} />
